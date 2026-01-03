efafb43bd6e9ea22e0fe83a3589c9bf594f767da
sabamisoyaki <86363290+sabamisoyaki@users.noreply.github.com>
2026-01-03 22:14:33 +0900

    Left-align game area with vertical playfield/next

--- src/tetris/view/GameView.java
+++ src/tetris/view/GameView.java
@@ -2,14 +2,11 @@ package tetris.view;
 
 import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class GameView {
 
     private final BorderPane root;
-    private final StackPane gameArea;
     private final NextPane nextPane;
     private final DeviceFramePane playFieldPane;
     private final CharacterPane characterPane;
@@ -17,13 +14,8 @@ public class GameView {
 
     public GameView() {
         this.root = new BorderPane();
-        this.gameArea = new StackPane();
-        double gameAreaWidth = 1440;
-        double gameAreaHeight = 1080;
-
-        gameArea.setPrefSize(gameAreaWidth, gameAreaHeight);
-        gameArea.setMinSize(gameAreaWidth, gameAreaHeight);
-        gameArea.setMaxSize(gameAreaWidth, gameAreaHeight);
+        double leftWidth = 1440;
+        double leftHeight = 1080;
 
         double playFieldSize = 900; // Wireframe square playfield size.
         double nextHeight = 180; // Wireframe 7:1 vertical split (900:180).
@@ -34,28 +26,23 @@ public class GameView {
         this.hudPane = new HudPane();
 
         VBox leftColumn = new VBox(playFieldPane, nextPane);
-        leftColumn.setAlignment(Pos.TOP_CENTER);
-        leftColumn.setPrefSize(playFieldSize, playFieldSize + nextHeight);
-
-        StackPane.setAlignment(leftColumn, Pos.CENTER);
-        gameArea.getChildren().add(leftColumn);
+        leftColumn.setAlignment(Pos.TOP_LEFT);
+        leftColumn.setPrefSize(leftWidth, leftHeight);
+        leftColumn.setMinSize(leftWidth, leftHeight);
+        leftColumn.setMaxSize(leftWidth, leftHeight);
 
         VBox sideArea = new VBox(characterPane, hudPane);
         sideArea.setPrefSize(480, 1080);
         sideArea.setMinSize(480, 1080);
         sideArea.setMaxSize(480, 1080);
 
-        root.setCenter(gameArea);
+        root.setLeft(leftColumn);
         root.setRight(sideArea);
         root.setStyle("-fx-background-color: black;");
     }
 
     public BorderPane getRoot() {
         return root;
-    }
-
-    public StackPane getGameArea() {
-        return gameArea;
     }
 
     public NextPane getNextPane() {
