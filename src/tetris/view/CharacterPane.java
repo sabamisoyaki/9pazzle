9fde85a05a8bb17166d069142ee422637b3f8972
sabamisoyaki <86363290+sabamisoyaki@users.noreply.github.com>
2026-02-01 16:24:08 +0900

    Add character image loading hook

--- src/tetris/view/CharacterPane.java
+++ src/tetris/view/CharacterPane.java
@@ -1,11 +1,20 @@
 package tetris.view;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class CharacterPane extends StackPane {
+
+    private static final Path DEFAULT_CHARACTER_IMAGE = Paths.get("images", "character.png");
 
     private final ImageView characterView;
 
@@ -25,9 +34,25 @@ public class CharacterPane extends StackPane {
         characterView.setPreserveRatio(true);
 
         getChildren().addAll(placeholder, characterView);
+
+        loadCharacterImage(DEFAULT_CHARACTER_IMAGE);
     }
 
     public ImageView getCharacterView() {
         return characterView;
+    }
+
+    public void loadCharacterImage(Path imagePath) {
+        if (imagePath == null || !Files.exists(imagePath)) {
+            characterView.setImage(null);
+            return;
+        }
+
+        try {
+            Image image = new Image(new FileInputStream(imagePath.toFile()));
+            characterView.setImage(image);
+        } catch (FileNotFoundException e) {
+            characterView.setImage(null);
+        }
     }
 }
